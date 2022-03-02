5401e748d625fe6493b6d4f47b58e61792670915
tpk256 <tpk256@mail.ru>
2022-03-03 01:12:45 +0500

    the end

--- LingVar.py
+++ LingVar.py
@@ -1,67 +1,58 @@
-class LingVar:
-    def __init__(self,name,terms,value):
-        self.name = name
+class Varling:
+    def __init__(self,terms,value,name_ling):
         self.terms = terms
         self.value = value
-
-        self.extremums = None
-        self.eps = None
-        self.aff_terms = None
-
-    def get_range(self):
+        self.name_ling = name_ling
+        self.definition_ranges_terms()
+        if not (value is None):
+            self.affilation_trapeze()
+    def definition_ranges_terms(self):
         ranges_terms = {}
         count_terms = len(self.terms)
+
+        #eps для значений,для которых функция принадлжености принимает значения > 0
         step = 100 // count_terms
+
+
+        #eps для ядра множества терма
+        core_step = step // 3 // 2
+
         for i in range(count_terms):
-            if i != count_terms - 1:
-                ranges_terms[self.terms[i]] = range(i * step + 1, (i + 1) * step + 1)
+            if i == 0:
+                core = range(1, i * step + 1 + step // 2 + core_step)
+                aff = range(1, (i + 1) * step + 1 + core_step)
+
+            elif i == count_terms - 1:
+                aff = range(ranges_terms[self.terms[i-1]]["core"][-1], 100 + 1)
+                core = range(i * step + 1 + step // 2-core_step,100 + 1 )
             else:
-                ranges_terms[self.terms[i]] = range(i * step + 1, 100 + 1)
+                aff = range(ranges_terms[self.terms[i-1]]["core"][-1], (i + 1) * step + 1+core_step)
+                core = range(i * step + 1 + step // 2 - core_step, i * step + 1 + step // 2 + core_step)
+            ranges_terms[self.terms[i]] = {"aff":aff,"core":core}
+        self.ranges_terms = ranges_terms
+
+    def affilation_trapeze(self):
+        self.affilation_terms = {}
+        for term_range in self.ranges_terms:
+            if self.value in self.ranges_terms[term_range]["aff"]:
+                if self.value in  self.ranges_terms[term_range]["core"]:
+                    self.affilation_terms[term_range] = 1.
+                else:
+
+                    if self.value < self.ranges_terms[term_range]["core"][0]:
+                        cur_val = self.value - self.ranges_terms[term_range]["aff"][0]
+                        self.affilation_terms[term_range] = cur_val / (self.ranges_terms[term_range]["core"][0]-self.ranges_terms[term_range]["aff"][0])
+                    else:
+                        cur_val = self.ranges_terms[term_range]["aff"][-1] - self.value
+                        self.affilation_terms[term_range] = cur_val / abs(self.ranges_terms[term_range]["aff"][-1] - self.ranges_terms[term_range]["core"][-1])
 
 
-        self.terms = ranges_terms
-
-    def get_affilation(self, diap, number):
-        # здесь diap есть экстремум
-
-        # Получаем степень принадлжености четкого X
-
-        if self.check_eps(diap, number):
-            return (self.eps - abs(diap - number)) / self.eps, number
-
-        return 0, number
-
-    def ready_aff(self):
-        extremums = self.extremums
-        number = self.value
-
-
-
-        result = {name_terms: 0 for name_terms in self.terms}
-        for ext, key in zip(extremums, result):
-            result[key] = self.get_affilation(ext, number)
-
-        self.aff_terms = result
-        # print(result)
-
-
-    def check_eps(self,diap, number):
-        #Проверка на вхождение в эпсилент окрестность
-
-        return abs(diap-number) <= self.eps
+            else:
+                self.affilation_terms[term_range] = 0.
 
 
 
-    def definition_eps_ext(self):
-        #Определение Эписилент и получение точек экстремумов
-        terms = self.terms
-        centers = []
-        for value in terms.values():
-            ext = (value[0] + value[-1]) // 2
-            centers.append(ext)
 
-        self.eps = sum(len(ranges) for ranges in terms.values()) / len(terms)
-        self.extremums = centers
 
 
     def __repr__(self):
